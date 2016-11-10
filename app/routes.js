d89ea9a784ee1699501f29c0b2fddc09775bdf07
Salveson <Andrew.Salveson@perkinswill.com>
2016-11-10 12:04:32 -0500

    add callback route

--- app/routes.js
+++ app/routes.js
@@ -2,4 +2,8 @@ module.exports = function(app){
   app.get('/',function(req,res){
     res.send('ok');
   });
+  app.get('/redirect',function(req,res){
+    console.log(req.body);
+    res.send('ok');
+  });
 };
